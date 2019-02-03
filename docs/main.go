4aa9a5c8621578c43e85c0f47268975c0dcdc5b7
deadcheat <deadcheat@users.noreply.github.com>
2019-02-04 06:47:34 +0900

    fixed generator to show json

--- docs/main.go
+++ docs/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"encoding/csv"
+	"encoding/json"
 	"log"
 	"os"
 	"strings"
+	"time"
+
+	"github.com/deadcheat/gegegengogogo"
 )
 
 func main() {
@@ -21,5 +25,22 @@ func main() {
 
 	for i := range records {
 		log.Println(strings.Join(records[i], ","))
+		loc, err := time.LoadLocation("Asia/Tokyo")
+		if err != nil {
+			panic(err)
+		}
+		from, err := time.ParseInLocation("2006/1/2", records[i][3], loc)
+		to, err := time.ParseInLocation("2006/1/2", records[i][4], loc)
+
+		log.Println(from.String(), to.String())
+		g := gegegengogogo.Gengo{
+			C:    gegegengogogo.GengoCodeFromString(records[i][0]),
+			Name: records[i][1],
+			Kana: records[i][2],
+			From: from,
+			To:   to,
+		}
+		d, _ := json.Marshal(g)
+		log.Println(string(d))
 	}
 }
