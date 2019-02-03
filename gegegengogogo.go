d5cf534c806d86e7d12f162f9d947678424f7adc
deadcheat <deadcheat@users.noreply.github.com>
2019-02-04 06:49:22 +0900

    added struct tags

--- gegegengogogo.go
+++ gegegengogogo.go
@@ -2,18 +2,11 @@ package gegegengogogo
 
 import "time"
 
-// GengoCode is a type alias for using Codes of 元号
-type GengoCode int
-
-const (
-	code GengoCode = iota
-)
-
 // Gengo is an entity of 元号
 type Gengo struct {
-	C    GengoCode
-	Name string
-	Kana string
-	From time.Time
-	To   time.Time
+	C    GengoCode `json:"code"`
+	Name string    `json:"name"`
+	Kana string    `json:"kana"`
+	From time.Time `json:"from"`
+	To   time.Time `json:"to"`
 }
