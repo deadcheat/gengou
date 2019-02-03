6c05b8acba34c00fbedb017d4e24978f51b380e1
deadcheat <deadcheat@users.noreply.github.com>
2019-02-03 13:57:57 +0900

    fixed list

--- docs/main.go
+++ docs/main.go
@@ -1,5 +1,25 @@
-package docs
+package main
+
+import (
+	"encoding/csv"
+	"log"
+	"os"
+	"strings"
+)
 
 func main() {
+	f, err := os.Open("list.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	r := csv.NewReader(f)
+	r.Comma = '\t'
+	records, err := r.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	for i := range records {
+		log.Println(strings.Join(records[i], ","))
+	}
 }
