a9da376c282369252e19ca3221929634d68f4377
deadcheat <deadcheat@users.noreply.github.com>
2019-02-13 21:12:27 +0900

    fixed to use added slice

--- gengou.go
+++ gengou.go
@@ -1,6 +1,9 @@
 package gengou
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 // Gengo is an entity of 元号
 type Gengo struct {
@@ -9,4 +12,36 @@ type Gengo struct {
 	Kana string    `json:"kana"`
 	From time.Time `json:"from"`
 	To   time.Time `json:"to"`
+}
+
+// SearchGengo searches gengo from given yyyy/mm/dd string
+func SearchGengo(date string) ([]Gengo, error) {
+	result := make([]Gengo, 0)
+	t, err := ParseSlashedYMD(date)
+	if err != nil {
+		return result, err
+	}
+
+	min := 0
+	max := len(gengoData)
+	for {
+		si := (min + max) / 2
+		s := gengoData[si]
+		fmt.Println(min, max, si, s.Name, t, s.From, s.To)
+		if s.To.After(t) && (s.From.Before(t) || s.From.Equal(t)) {
+			result = append(result, s)
+		}
+
+		if (max - min) <= 1 {
+			break
+		}
+
+		if s.To.After(t) {
+			max = si
+			continue
+		}
+		min = si
+	}
+
+	return result, nil
 }
